d2ed4c90972191d3d5b3f49c2eb58c0966b20a36
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-10 20:59:12 +0800

    Added slide up

--- main.js
+++ main.js
@@ -6,6 +6,21 @@
         opacity: 1 !important;
         transition: 1s ease;
     }
+    .slideUp {
+        opacity: 1 !important;
+        transition: 1s ease !important;
+        animation: slideUp 1s ease 1 !important;
+    }
+
+    @keyframes slideUp {
+        0% {
+            transform: translateY(30px);
+        }
+
+        100% {
+            transform: translateY(0px);
+        }
+    }
     </style>
     <div id="enfold">
         <slot>
@@ -21,7 +36,6 @@
       this.shadowRoot.appendChild(template.content.cloneNode(true));
 
       const root = this.shadowRoot.querySelector("#enfold");
-      root.style.opacity = 0;
       console.log(root);
 
       const observer = new IntersectionObserver((entries) => {
