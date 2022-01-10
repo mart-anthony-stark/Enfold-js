ac4e446b8bec8481378ee6bc1764098b9057649d
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-10 21:27:32 +0800

    bundle

--- build/bundle.js
+++ build/bundle.js
@@ -13,14 +13,31 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var EnfoldAnimate = /** @class */ (function (_super) {
-    __extends(EnfoldAnimate, _super);
-    function EnfoldAnimate() {
-        var _this = _super.call(this) || this;
-        _this.innerHTML = "<h1>Hello</h1>";
-        return _this;
-    }
-    return EnfoldAnimate;
-}(HTMLElement));
-window.customElements.define("enfold-animate", EnfoldAnimate);
+(function Enfold() {
+    var EnfoldAnimate = /** @class */ (function (_super) {
+        __extends(EnfoldAnimate, _super);
+        function EnfoldAnimate() {
+            var _this = _super.call(this) || this;
+            var animationType = _this.getAttribute("animation");
+            var animationDelay = parseInt(_this.getAttribute("delay"));
+            var animationDuration = parseInt(_this.getAttribute("duration"));
+            var easing = _this.getAttribute("easing");
+            var template = document.createElement("template");
+            template.innerHTML = "\n        <style>\n        #enfold{\n            animation-duration: ".concat(animationDuration | 1, "s !important;\n            animation-timing-function: ").concat(easing ? easing : "ease", " !important;\n            animation-delay: ").concat(animationDelay | 0, "s !important;\n        }\n        .fadeIn {\n            animation-name: fadeIn !important;\n        }\n        .slideUp {\n            animation-name: slideUp !important;\n        }\n\n        @keyframes fadeIn {\n            0% {\n                opacity: 0;\n            }\n            100% {\n                opacity: 1;\n            }\n        }\n        @keyframes slideUp {\n            0% {\n                opacity: 0;\n                transform: translateY(30px);\n            }\n            100% {\n                opacity: 1;\n                transform: translateY(0px);\n            }\n        }\n        </style>\n        <div id=\"enfold\">\n            <slot>\n        </div>\n    ");
+            _this.attachShadow({ mode: "open" });
+            _this.shadowRoot.appendChild(template.content.cloneNode(true));
+            var root = _this.shadowRoot.querySelector("#enfold");
+            console.log(root);
+            var observer = new IntersectionObserver(function (entries) {
+                entries.forEach(function (entry) {
+                    entry.target.classList.toggle(animationType, entry.isIntersecting);
+                }, { threshold: 0.5 });
+            });
+            observer.observe(root);
+            return _this;
+        }
+        return EnfoldAnimate;
+    }(HTMLElement));
+    window.customElements.define("enfold-animate", EnfoldAnimate);
+})();
 //# sourceMappingURL=bundle.js.map
