d8974feaf484d4ac061045491b525d4fa6e9df4a
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-11 14:23:10 +0800

    Add zoomIn

--- main.js
+++ main.js
@@ -3,10 +3,17 @@
     constructor() {
       super();
       const animationType = this.getAttribute("name");
-      const animationDelay = this.getAttribute("delay") | 0;
-      const animationDuration = this.getAttribute("duration") | 1;
-      const easing = this.getAttribute("easing") | "ease";
-      const intensity = this.getAttribute("intensity") | 30;
+      const animationDelay = this.getAttribute("delay")
+        ? this.getAttribute("delay")
+        : 0;
+      const animationDuration = this.getAttribute("duration")
+        ? this.getAttribute("duration")
+        : 1;
+      const easing = this.getAttribute("easing")
+        ? this.getAttribute("easing")
+        : "ease";
+      const intensity = this.getAttribute("intensity") | 100;
+      const threshold = this.getAttribute("threshold") | 0.5;
       const once = this.hasAttribute("once");
 
       const template = document.createElement("template");
@@ -32,6 +39,18 @@
         .slideRight {
             animation-name: slideRight !important;
         }
+        .zoomIn {
+            animation-name: zoomIn !important;
+        }
+        .zoomInLeft {
+            animation-name: zoomInLeft !important;
+        }
+        .zoomInRight {
+            animation-name: zoomInRight !important;
+        }
+        .${animationType}-leave {
+            animation-name: ${animationType}-leave !important;
+        }
 
         @keyframes fadeIn {
             0% {
@@ -41,6 +60,16 @@
                 opacity: 1;
             }
         }
+
+        @keyframes fadeIn-leave{
+            0% {
+                opacity: 1;
+            }
+            100% {
+                opacity: 0;
+            }
+        }
+
         @keyframes slideUp {
             0% {
                 transform: translateY(${intensity}px);
@@ -73,6 +102,32 @@
                 transform: translateY(0px);
             }
         }
+
+        @keyframes zoomIn {
+            0% {
+                transform: scale(0.5);
+
+            }
+            100% {
+                transform: scale(1);
+            }
+        }
+        @keyframes zoomInLeft {
+            0% {
+                transform: translateX(${intensity}px) scale(0.5);
+            }
+            100% {
+                transform: translateX(0px) scale(1);
+            }
+        }
+        @keyframes zoomInRight {
+            0% {
+                transform: translateX(${-intensity}px) scale(0.5);
+            }
+            100% {
+                transform: translateX(0px) scale(1);
+            }
+        }
         </style>
         <div id="enfold">
             <slot>
@@ -83,20 +138,28 @@
       this.shadowRoot.appendChild(template.content.cloneNode(true));
 
       const root = this.shadowRoot.querySelector("#enfold");
+      root.style.opacity = 0;
 
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach(
-          (entry) => {
-            entry.target.classList.toggle(animationType, entry.isIntersecting);
-            if (once) {
-              observer.unobserve(entry.target);
-            }
-          },
-          { threshold: 0.5 }
-        );
+      const observer = new IntersectionObserver(callback, {
+        threshold,
       });
 
       observer.observe(root);
+
+      function callback(entries) {
+        entries.forEach((entry) => {
+          entry.target.classList.toggle(animationType, entry.isIntersecting);
+          entry.target.classList.toggle(
+            animationType + "-leave",
+            !entry.isIntersecting
+          );
+
+          if (once) {
+            observer.unobserve(entry.target);
+          }
+          root.style.opacity = 1;
+        });
+      }
     }
   }
 
