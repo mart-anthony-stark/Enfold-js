6a4ba88f21f8726d3d8f25de9d64313f5315c53c
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-11 20:27:59 +0800

    Added fade animations

--- src/main.ts
+++ src/main.ts
@@ -19,115 +19,134 @@
       const template = document.createElement("template");
       template.innerHTML = `
           <style>
-          #enfold{
-              animation-duration: ${animationDuration}s !important;
-              animation-timing-function: ${easing} !important;
-              animation-delay: ${animationDelay}s !important;
-          }
-          .fadeIn {
-              animation-name: fadeIn !important;
-          }
-          .slideUp {
-              animation-name: slideUp !important;
-          }
-          .slideLeft {
-              animation-name: slideLeft !important;
-          }
-          .slideDown {
-              animation-name: slideDown !important;
-          }
-          .slideRight {
-              animation-name: slideRight !important;
-          }
-          .zoomIn {
-              animation-name: zoomIn !important;
-          }
-          .zoomInLeft {
-              animation-name: zoomInLeft !important;
-          }
-          .zoomInRight {
-              animation-name: zoomInRight !important;
-          }
-          .${animationType}-leave {
-              animation-name: ${animationType}-leave !important;
-          }
-  
-          @keyframes fadeIn {
-              0% {
-                  opacity: 0;
-              }
-              100% {
-                  opacity: 1;
-              }
-          }
-  
-          @keyframes fadeIn-leave{
-              0% {
-                  opacity: 1;
-              }
-              100% {
-                  opacity: 0;
-              }
-          }
-  
-          @keyframes slideUp {
-              0% {
-                  transform: translateY(${intensity}px);
-              }
-              100% {
-                  transform: translateY(0px);
-              }
-          }
-          @keyframes slideLeft {
-              0% {
-                  transform: translateX(${intensity}px);
-              }
-              100% {
-                  transform: translateX(0px);
-              }
-          }
-          @keyframes slideRight {
-              0% {
-                  transform: translateX(-${intensity}px);
-              }
-              100% {
-                  transform: translateX(0px);
-              }
-          }
-          @keyframes slideDown {
-              0% {
-                  transform: translateY(-${intensity}px);
-              }
-              100% {
-                  transform: translateY(0px);
-              }
-          }
-  
-          @keyframes zoomIn {
-              0% {
-                  transform: scale(0.5);
-  
-              }
-              100% {
-                  transform: scale(1);
-              }
-          }
-          @keyframes zoomInLeft {
-              0% {
-                  transform: translateX(${intensity}px) scale(0.5);
-              }
-              100% {
-                  transform: translateX(0px) scale(1);
-              }
-          }
-          @keyframes zoomInRight {
-              0% {
-                  transform: translateX(${-intensity}px) scale(0.5);
-              }
-              100% {
-                  transform: translateX(0px) scale(1);
-              }
-          }
+          <style>
+        #enfold{
+            animation-duration: ${animationDuration}s !important;
+            animation-timing-function: ${easing} !important;
+            animation-delay: ${animationDelay}s !important;
+        }
+        .fadeIn {
+            animation-name: fadeIn !important;
+        }
+        .fadeLeft {
+            animation-name: fadeLeft !important;
+        }
+        .fadeRight {
+            animation-name: fadeRight !important;
+        }
+        .slideUp {
+            animation-name: slideUp !important;
+        }
+        .slideLeft {
+            animation-name: slideLeft !important;
+        }
+        .slideDown {
+            animation-name: slideDown !important;
+        }
+        .slideRight {
+            animation-name: slideRight !important;
+        }
+        .zoomIn {
+            animation-name: zoomIn !important;
+        }
+        .zoomInLeft {
+            animation-name: zoomInLeft !important;
+        }
+        .zoomInRight {
+            animation-name: zoomInRight !important;
+        }
+        .${animationType}-leave {
+            animation-name: ${animationType}-leave !important;
+        }
+
+        @keyframes fadeIn {
+            0% {
+                opacity: 0;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+        @keyframes fadeLeft {
+            0% {
+                transform: translateX(${intensity}px);
+                opacity: 0;
+            }
+            100% {
+                transform: translateX(0px);
+                opacity: 1;
+            }
+        }
+        @keyframes fadeRight {
+            0% {
+                transform: translateX(-${intensity}px);
+                opacity: 0;
+            }
+            100% {
+                transform: translateX(0px);
+                opacity: 1;
+            }
+        }
+
+        @keyframes slideUp {
+            0% {
+                transform: translateY(${intensity}px);
+            }
+            100% {
+                transform: translateY(0px);
+            }
+        }
+        @keyframes slideLeft {
+            0% {
+                transform: translateX(${intensity}px);
+            }
+            100% {
+                transform: translateX(0px);
+            }
+        }
+        @keyframes slideRight {
+            0% {
+                transform: translateX(-${intensity}px);
+            }
+            100% {
+                transform: translateX(0px);
+            }
+        }
+        @keyframes slideDown {
+            0% {
+                transform: translateY(-${intensity}px);
+            }
+            100% {
+                transform: translateY(0px);
+            }
+        }
+
+        @keyframes zoomIn {
+            0% {
+                transform: scale(0.5);
+
+            }
+            100% {
+                transform: scale(1);
+            }
+        }
+        @keyframes zoomInLeft {
+            0% {
+                transform: translateX(${intensity}px) scale(0.5);
+            }
+            100% {
+                transform: translateX(0px) scale(1);
+            }
+        }
+        @keyframes zoomInRight {
+            0% {
+                transform: translateX(${-intensity}px) scale(0.5);
+            }
+            100% {
+                transform: translateX(0px) scale(1);
+            }
+        }
+        </style>
           </style>
           <div id="enfold">
               <slot>
